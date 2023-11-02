a998904eea2f27e1a101f312302d6a3e6e826db8
ibajana <isaiasbajana@gmail.com>
2023-11-02 13:15:24 -0500

    exercise 6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,17 +1,11 @@
 const leapYears = function(year) {
-    if (year%4==0){
+    if (year%4==0 && (year%100!=0 || year%400==0)){
         return true
-    }:
-    if(year%100==0){
+    }
+    else{
         return false
-    }:
-    if (year%400==0 && year%100==0){
-        true
-    };
+    }
     
-    else {
-        return false
-    };
 };
 
 // Do not edit below this line
