9ff882e5a67b1513b4015f73966a810b7f4d6e51
ibajana <isaiasbajana@gmail.com>
2023-11-02 14:26:19 -0500

    completed to exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const convertToCelsius = function() {
-  
+const convertToCelsius = function(fahrenheit) {
+  let result= (fahrenheit-32)*(5/9)
+  return parseFloat(result.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let result= (celcius*(9/5))+32
+  return parseFloat(result.toFixed(1))
 };
 
 // Do not edit below this line
